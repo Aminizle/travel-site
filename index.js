97a7c9a5da35269526cdc856e20219b4cdbff1e3
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2025-04-20 22:56:09 -0400

    updated links

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const menuToggle = document.querySelector(".toggle");
-const showcaseToggle = document.querySelector(".showcase");
-const homeToggle = document.querySelector(".home");
+const showcase = document.querySelector(".showcase");
+const menu = document.querySelector(".menu");
+const homeLink = document.getElementById("homeLink");
+
+
+homeLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".menu").classList.remove("open");
+  document.querySelector(".toggle").classList.remove("active");
+  document.querySelector(".showcase").classList.remove("active");
+
+  // Optional: scroll to top or to the showcase section
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
-});
-showcaseToggle.addEventListener("click", () => {
-  showcaseToggle.classList.toggle("active");
-});
-homeToggle.addEventListener("click", () => {
-  showcaseToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+  menu.classList.toggle("open");
 });
